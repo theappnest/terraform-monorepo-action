6c9ee7211dc8909af881f9224166124bd6a072af
Adam Bouqdib <adam@abemedia.co.uk>
2021-08-06 13:47:42 +0100

    feat: support workflow dispatch event (#26)
    * feat: support workflow dispatch event
    
    * chore: build
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,61 @@
+import { context, getOctokit } from '@actions/github'
+import {
+  PullRequestEvent,
+  PushEvent,
+  WorkflowDispatchEvent,
+} from '@octokit/webhooks-types'
+
+export async function getSha(
+  token: string,
+): Promise<{ head: string; base: string }> {
+  const octokit = getOctokit(token)
+
+  let base: string | undefined
+  let head: string | undefined
+
+  switch (context.eventName) {
+    case 'pull_request': {
+      const payload = context.payload as PullRequestEvent
+      base = payload.pull_request?.base?.sha
+      head = payload.pull_request?.head?.sha
+      break
+    }
+    case 'push': {
+      const payload = context.payload as PushEvent
+      base = payload.before
+      head = payload.after
+      break
+    }
+    case 'workflow_dispatch': {
+      const payload = context.payload as WorkflowDispatchEvent
+
+      const ref = await octokit.rest.git.getRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: payload.ref,
+      })
+
+      const commit = await octokit.rest.git.getCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: ref.data.object.sha,
+      })
+
+      base = commit.data.parents[0].sha
+      head = commit.data.sha
+      break
+    }
+    default:
+      throw new Error(`Unsupported event: ${context.eventName}`)
+  }
+
+  if (!base || !head) {
+    throw new Error('Refs not found')
+  }
+
+  return { base, head }
+}
+
 export function getModulePaths<T extends Record<string, unknown>>(
   files: T[] | undefined,
   pathProp: keyof T,
